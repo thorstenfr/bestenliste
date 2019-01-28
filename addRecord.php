bbf409d22cd3d5042780d08fc6fbfdc3415d4804
Gastbenutzer <Guest@Thorstens-Mac-mini.local>
2019-01-28 23:17:54 +0100

    Datensätze können geschrieben werden.

--- addRecord.php
+++ addRecord.php
@@ -1,3 +1,28 @@
 <?php
-	echo "Hello World.";
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "bestenliste";
+	
+	
+	
+	// Verbindung herstellen
+	$conn = mysqli_connect($servername,$username,$password,$dbname);
+	// Verbindung prüfen
+	if (!$conn){
+		die("Verbindung fehlgeschlagen: " . mysqli_connect_error());		
+	}
+	
+	// prepare and bind
+	$stmt = $conn->prepare("INSERT INTO scores (spieler, punkte) VALUES (?, ?)");
+	$stmt->bind_param("si", $spieler, $punkte);
+
+	// set parameters and execute
+	$spieler = $_POST['spieler'];
+	$punkte = intval($_POST['punkte']);	
+	$stmt->execute();
+	
+	// Verbindung explizit schließen
+	mysqli_close($conn);
+		
 ?>
